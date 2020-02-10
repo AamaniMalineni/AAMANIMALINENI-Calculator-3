baba7dfa8351ac159768ad00c145ce78e49fdb4b
AamaniMalineni <50495275+AamaniMalineni@users.noreply.github.com>
2020-02-10 18:14:14 +0530

    Add files via upload

---
+++ Calculator.java
@@ -0,0 +1,26 @@
+package epam.com.calculator;
+
+public class Calculator {
+	public static int add(int a,int b) {
+		return a+b;
+	}
+	public static int mul(int a,int b) {
+		return a*b;
+	}
+	public static int sub(int a,int b) {
+		return a-b;
+	}
+	public static int div(int a,int b) {
+		try {
+		return a/b;
+	}
+		catch(ArithmeticException e) {
+			System.out.println(e);
+			return -1;
+		}
+	}
+	public static int modulodivision(int a,int b) {
+		return a%b;
+	}
+
+}
